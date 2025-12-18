2c64ad78ccd1e88226ddbcca8393ba98f62e2532
Mark D <mjtdaams@gmail.com>
2025-12-18 13:52:52 -0500

    Tweaks
    Added a config file.
    Removed white space around logo image.
    Match track layout to background image.

--- ui/src/app/config/config.ts
+++ ui/src/app/config/config.ts
@@ -1,8 +1,12 @@
-export const STROKE_WIDTH = 30;
-export const INDICATOR_LENGTH = 100;
-export const START_POS_INDICATOR_RADIUS = 100;
-export const TRACK_COLOR = "orange";
+// Config for UI
 
-export const LAYOUT_START_X = 3910;
+// Start position for track layout on the viewbox/map/world
+export const LAYOUT_START_X = 3916;
 export const LAYOUT_START_Y = 2700;
 export const LAYOUT_START_HEADING = 0;
+
+// Track drawing
+export const STROKE_WIDTH = 24;
+export const INDICATOR_LENGTH = 100;
+export const START_POS_INDICATOR_RADIUS = 100;
+export const TRACK_COLOR = "blue";
