fe01575cb7149a983035d797c9fa326f50a3e6b6
Mark D <mjtdaams@gmail.com>
2025-12-18 17:17:43 -0500

    Fixed the layout. Added getStartCoordinate

--- server/src/track/calculations.ts
+++ server/src/track/calculations.ts
@@ -2,15 +2,22 @@ import { Coordinate } from "trainbrain-shared";
 import { LayoutPiece } from "./layout.js";
 import { TrackPieceDef } from "./piecedefinitions.js";
 
-// Returns the end coordinates and angle of a track piece based on the previous piece
-// and the current piece definition.
+/**
+ * Returns the end coordinate and heading of a track piece based on
+ * a known start coordinate and the current piece's definition.
+ *
+ * @param layoutPiece {LayoutPiece} - The information about the track piece from the layout json file
+ * @param pieceDef {TrackPieceDef} - The definition of the track piece (contains info like length, angle, etc)
+ * @param startCoordinate {Coordinate} - The x pos, y pos, and heading of the start position of the track piece.
+ *
+ * @returns {Coordinate} - The x pos, y pos, and heading of the end position of this track piece.
+ */
 export function getEndCoordinate(
   layoutPiece: LayoutPiece,
   pieceDef: TrackPieceDef,
   startCoordinate: Coordinate
 ): Coordinate
 {
-  // Calculate the new coordinates based on the piece definition
   let dX = 0;
   let dY = 0;
   let pieceAngle = 0;
@@ -54,6 +61,68 @@ export function getEndCoordinate(
     x: startCoordinate.x + dX,
     y: startCoordinate.y + dY,
     heading: startCoordinate.heading + pieceAngle,
+  }
+}
+
+/**
+ * Returns the start coordinate and heading of a track piece based on
+ * a known end coordinate and the current piece's definition.
+ *
+ * @param layoutPiece {LayoutPiece} - The information about the track piece from the layout json file
+ * @param pieceDef {TrackPieceDef} - The definition of the track piece (contains info like length, angle, etc)
+ * @param endCoordinate {Coordinate} - The x pos, y pos, and heading of the end position of the track piece.
+ *
+ * @returns {Coordinate} - The x pos, y pos, and heading of the start position of this track piece.
+ */
+export function getStartCoordinate(
+  layoutPiece: LayoutPiece,
+  pieceDef: TrackPieceDef,
+  endCoordinate: Coordinate
+): Coordinate
+{
+  let dX = 0;
+  let dY = 0;
+  let pieceAngle = 0;
+
+  switch(pieceDef.type) {
+    case "straight":
+      const length = pieceDef.length as number;
+      const heading = endCoordinate.heading;
+
+      // Calculate x and y position based on the heading of the track piece
+      dX = roundTo2(length * Math.sin(degreesToRadians(heading)));
+      dY = roundTo2(length * Math.cos(degreesToRadians(heading)));
+      break;
+    case "curve":
+      const radius = pieceDef.radius as number;
+      pieceAngle = pieceDef.angle as number;
+
+      // Calculate x and y position based on the angle of the track piece
+      dX = radius * (1 - Math.cos(degreesToRadians(pieceAngle)));
+      dY = radius * Math.sin(degreesToRadians(pieceAngle));
+
+      // Invert the angle and the x-coordinate if the piece is facing left instead of right
+      if (layoutPiece.direction == "left") {
+        dX = (0 - dX);
+        pieceAngle = (0 - pieceAngle);
+      }
+
+      // Rotate the track piece to fit correctly on the end of the previous piece
+      const rotated = rotatePoint(dX, dY, endCoordinate.heading);
+
+      // Round the values to two decimal points
+      dX = roundTo2(rotated.x);
+      dY = roundTo2(rotated.y);
+      break;
+    default:
+      throw new Error(`Unknown piece type: ${pieceDef.type}`);
+  }
+
+  // Assign the x, y and heading based on the previous calculations
+  return {
+    x: endCoordinate.x - dX,
+    y: endCoordinate.y - dY,
+    heading: endCoordinate.heading - pieceAngle,
   }
 }
 
