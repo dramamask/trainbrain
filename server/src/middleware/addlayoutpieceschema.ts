3771f2420a8b5cf2a36b36799a11179f6594dab1
Mark D <mjtdaams@gmail.com>
2026-01-04 18:12:19 -0500

    Fixed validation messages.
    Still needs debugging. Has errors.

--- server/src/middleware/addlayoutpieceschema.ts
+++ server/src/middleware/addlayoutpieceschema.ts
@@ -4,12 +4,16 @@ import { pieceDefintionsDb } from '../services/db.js';
 
 export const addLayoutPieceSchema = [
   body('connectionName')
-    .notEmpty().isString().isIn(["start", "end", "diverge"])
-    .withMessage("JSON parameter 'connectionName' is required and should either have one of the following values: 'start', 'end', 'diverge'"),
+    .notEmpty().isString()
+    .withMessage("JSON parameter 'connectionName' should be a string")
+    .isIn(["start", "end", "diverge"])
+    .withMessage("JSON parameter 'connectionName' should have one of the following values: 'start', 'end', 'diverge'"),
 
   body('connectToPiece')
-    .notEmpty().isString().toInt().isInt({ min: 0 })
-    .withMessage("JSON parameter 'connectToPiece' is required and should be a string representation of a numeric value greater than or equal to 0")
+    .notEmpty().isString()
+    .withMessage("JSON parameter 'connectToPiece' should be a string")
+    .toInt().isInt({ min: 0 })
+    .withMessage("JSON parameter 'connectToPiece' should be a string representation of a numeric value greater than or equal to 0")
     .custom((id: number) => {
       const highestIdAllowed = layout.getHighestPieceId();
       if (id > highestIdAllowed) {
@@ -20,7 +24,7 @@ export const addLayoutPieceSchema = [
 
   body('pieceDefId')
     .notEmpty().isString()
-    .withMessage("JSON parameter 'pieceDefId' is required and should be a string")
+    .withMessage("JSON parameter 'pieceDefId' should be a string")
     .custom((id: string) => {
       if (id in pieceDefintionsDb.data.definitions) {
         return true;
