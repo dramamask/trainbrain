0d27c028467a5b3b37ea53bc00822e5a0a90222b
Mark D <mjtdaams@gmail.com>
2026-01-11 17:38:22 -0500

    Updated keyboard shortcut UI
    Better spacing

--- ui/src/app/components/controlssection/keyboardshortcuts.tsx
+++ ui/src/app/components/controlssection/keyboardshortcuts.tsx
@@ -1,3 +1,4 @@
+import { JSX } from "@emotion/react/jsx-runtime";
 import { capitalCase } from "change-case";
 import { Card, CardContent, Grid, Stack } from "@mui/material";
 import { KEY } from "@/app/services/keyeventhandlers/keydefinitions";
@@ -9,22 +10,44 @@ export default function KeyboardShortcuts() {
   return (
       <Card className={styles.card}>
         <CardContent className={styles.cardContent}>
-          <Stack className={styles.stack}>
             <div className={csStyles.title + " " + styles.heading}>Keyboard Shortcuts</div>
-            {
-              Object.entries(KEY).flatMap(([key, value]) => {
-                return (
-                    <Grid container key={key}>
-                      <Grid className={csStyles.text} size={3.5}><b>{getKeyName(value)}</b></Grid>
-                      <Grid className={csStyles.text + " " + styles.text} size="grow">{capitalCase(key)}</Grid>
-                    </Grid>
-                )
-              })
-            }
-          </Stack>
+            <Grid container spacing={1}>
+              <Grid size="auto">
+                <Stack>{ renderKeys(KEY) }</Stack>
+              </Grid>
+              <Grid size="grow">
+                <Stack>{ renderDescriptions(KEY) }</Stack>
+              </Grid>
+            </Grid>
         </CardContent>
       </Card>
     )
+}
+
+// Render the list of keys for the keyboard shortcut list
+function renderKeys(keyObjectList: object): JSX.Element[] {
+  const listItems: JSX.Element[] = [];
+
+  Object.entries(keyObjectList).flatMap(([key, value]) => {
+    listItems.push(
+      <div key={key} className={csStyles.text}><b>{getKeyName(value)}</b></div>
+    )
+  });
+
+  return listItems;
+}
+
+// Render the list of descriptions for the keyboard shortcut list
+function renderDescriptions(keyObjectList: object): JSX.Element[] {
+  const listItems: JSX.Element[] = [];
+
+  Object.entries(keyObjectList).flatMap(([key, value]) => {
+    listItems.push(
+      <div className={csStyles.text}>{capitalCase(key)}</div>
+    )
+  });
+
+  return listItems;
 }
 
 // Make sure we handle things well when keyName is an array
